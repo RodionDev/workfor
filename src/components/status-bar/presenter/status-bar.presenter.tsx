ea97937307ba89630b5fb24e5b6f001e9f4562c9
Rodion <rodion.developer1@gmail.com>
2021-12-11 15:37:20 +0000

    Add status bar component

--- src/components/status-bar/presenter/status-bar.presenter.tsx
+++ src/components/status-bar/presenter/status-bar.presenter.tsx
@@ -1,9 +1,39 @@
 import * as React from 'react';
 import { WithStyles, withStyles } from '@material-ui/core/styles';
 import styles from './status-bar.styles';
-interface Props extends WithStyles<typeof styles> {}
-const StatusBarPresenter: React.SFC<Props> = ({classes}) => 
-  <div className={classes.root}>
-    Status Bar
-  </div>
+import { Tabs, Tab, Paper } from '@material-ui/core';
+interface Props extends WithStyles<typeof styles> {
+}
+interface State {
+  value: number
+}
+const StatusBarPresenter = withStyles(styles)(
+  class extends React.Component<Props, State> {
+    state = {
+      value: 0,
+    };
+    handleChange = (_: any, value: any) => {
+      this.setState({ value })
+    };
+    render(): JSX.Element {
+      const { classes } = this.props;
+      return (
+        <Paper color='default' className={classes.root}>
+          <Tabs
+            value={this.state.value}
+            onChange={this.handleChange}
+            indicatorColor='secondary'
+            textColor='secondary'
+            centered={true}
+          >
+            <Tab label='778' icon={<span className={classes.tabTitle}>Bài viết</span>}className={classes.noCap}/>
+            <Tab label='17.2N' icon={<span className={classes.tabTitle}>Thích</span>} className={classes.noCap}/>
+            <Tab label='7.3N' icon={<span className={classes.tabTitle}>Follower</span>} className={classes.noCap}/>
+            <Tab label='221' icon={<span className={classes.tabTitle}>Following</span>} className={classes.noCap}/>
+          </Tabs>
+        </Paper>
+    );
+    }
+  }
+)
 export default withStyles(styles)(StatusBarPresenter);
--- src/components/status-bar/presenter/status-bar.styles.ts
+++ src/components/status-bar/presenter/status-bar.styles.ts
@@ -2,7 +2,18 @@ import { createStyles, Theme } from '@material-ui/core/styles';
 const styles  = (theme: Theme) => createStyles({
   root: {
     flex: 1,
-    minHeight: 48
+    flexGrow: 1,
+    minHeight: 48,
+    backgroundColor: theme.palette.background.paper,
+  },
+  noCap: {
+    textTransform: 'none',
+    fontSize: 17,
+    fontWeight: 800
+  },
+  tabTitle: {
+    fontSize: 12,
+    fontWeight: 500
   }
 });
 export default styles;
