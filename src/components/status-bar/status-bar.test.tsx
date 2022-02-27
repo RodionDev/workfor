3c82e64f2d736db5aa6226150ce50239a916a69e
Rodion <rodion.developer1@gmail.com>
2022-02-27 01:01:39 +0000

    update unit test

--- src/components/status-bar/status-bar.test.tsx
+++ src/components/status-bar/status-bar.test.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import StatusBarPresenter from './presenter/status-bar.presenter';
-const dummy = () => {
-  const a = 1;
-};
+const dummy = () => {};
 it('StatusBar renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<StatusBarPresenter handleTabChange={dummy}/>, div);
