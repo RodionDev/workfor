5b28865ac4048187dc7e42dc532bad50c5171de1
Rodion <rodion.developer1@gmail.com>
2022-07-08 08:38:01 +0000

    update

--- src/components/follower/follower.test.tsx
+++ src/components/follower/follower.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Follower } from '..';
+import  Follower  from './presenter/follower.presenter';
 it('StatusBar renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Follower data={{}}/>, div);
--- src/components/primary-panel/primary-panel.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import PrimaryPanelPresenter from './presenter/primary-panel.presenter';
-it('PrimaryPanel renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<PrimaryPanelPresenter tabId={1}/>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
